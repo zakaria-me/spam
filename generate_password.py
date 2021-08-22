8f155f404191677f8c59d10922475a87cde672b1
Zakaria M <zakaria.mellah.pro@gmail.con>
2021-08-22 15:27:28 +0200

    rename main.py to generate_password.py

--- generate_password.py
+++ generate_password.py
@@ -9,8 +9,6 @@ def set_characters_set(character_set_number):
     all_characters = string.ascii_letters + string.digits + string.punctuation
   return all_characters
 
-generated_password = ""
-
 def set_password_length(password_length):
   password_length= input("What length do you want your password to be? ")
   if password_length.isdigit() and int(password_length) <= 8:
@@ -23,9 +21,14 @@ def set_password_length(password_length):
     password_length = int(password_length)
   return password_length
 
-for i in range(set_password_length):
-  random_int = random.randint(0, len(set_characters_set) -1)
-  generated_password += set_characters_set[random_int]
+def generate_password(character_set, password_length):
+  character_set = set_characters_set
+  password_length = set_password_length
+  generated_password = ""
+  for i in range(password_length):
+    random_int = random.randint(0, len(character_set) -1)
+    generated_password += character_set[random_int]
+  return generated_password
 
 print("Here is your generated password:")
-print(generated_password)
+print(generate_password)
