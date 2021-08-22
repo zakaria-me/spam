7e3e535c5b4bb69aa643f658dcd6c799dd79df50
Zakaria M <zakaria.mellah.pro@gmail.con>
2021-08-22 15:17:59 +0200

    factorize the main script

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import string, random
 
-all_characters = string.ascii_letters + string.digits + string.punctuation
+def set_characters_set(character_set_number):
+  if character_set_number == 1:
+    all_characters = string.ascii_letters
+  elif character_set_number == 2:
+    all_characters = string.ascii_letters + string.digits
+  elif character_set_number == 3:
+    all_characters = string.ascii_letters + string.digits + string.punctuation
+  return all_characters
+
 generated_password = ""
 
-password_length= input("What length do you want your password to be? ")
-if password_length.isdigit() and int(password_length) <= 8:
-  print("The length must be greater than 8")
-  exit()
-elif password_length.isdigit() == False:
-  print("The length must be an integer")
-  exit()
-else:
-  password_length = int(password_length)
+def set_password_length(password_length):
+  password_length= input("What length do you want your password to be? ")
+  if password_length.isdigit() and int(password_length) <= 8:
+    print("The length must be greater than 8")
+    exit()
+  elif password_length.isdigit() == False:
+    print("The length must be an integer")
+    exit()
+  else:
+    password_length = int(password_length)
+  return password_length
 
-for i in range(password_length):
-  random_int = random.randint(0, len(all_characters) -1)
-  generated_password += all_characters[random_int]
+for i in range(set_password_length):
+  random_int = random.randint(0, len(set_characters_set) -1)
+  generated_password += set_characters_set[random_int]
 
 print("Here is your generated password:")
 print(generated_password)
