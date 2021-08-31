5be82ea12628aab718c88c10ce142d0cf127df27
Zakaria M <zakaria.mellah.pro@gmail.con>
2021-08-31 21:54:38 +0200

    add password authentification to open the app

---
+++ password_generator/application_password.py
@@ -0,0 +1,29 @@
+import os
+import hashlib
+
+def generate_key(password):
+  salt = os.urandom(32)
+
+  key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+
+  path = get_path()
+
+  with (open(path, mode='bw')) as password_key:
+    password_key.write(salt + key)
+
+def verify_key(password):
+  path = get_path()
+  print(password)
+  with (open(path, mode="rb")) as password_key:
+    salt = password_key.read(32)
+    key=password_key.read()
+  key_of_provided_password= hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+  if key == key_of_provided_password:
+    # open gui
+    return True
+  else:
+    return False
+
+def get_path():
+  path = "password_generator/password_key.txt"
+  return path
